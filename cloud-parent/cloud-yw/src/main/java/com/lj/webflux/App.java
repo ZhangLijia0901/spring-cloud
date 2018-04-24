648df8fdc6f0ab0349a6d814966c59a558b21d71
zhanglijia <2495205566@qq.com>
2018-04-24 13:32:52 +0800

    boot + mybatis

--- cloud-parent/cloud-yw/src/main/java/com/lj/webflux/App.java
+++ cloud-parent/cloud-yw/src/main/java/com/lj/webflux/App.java
@@ -3,27 +3,48 @@ package com.lj.webflux;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactoryUtils;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.http.MediaType;
 
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 
+//@WxApplication
 @SpringBootApplication
-public class App implements InitializingBean {
+public class App implements InitializingBean, ApplicationContextAware {
 
-	@Autowired
-	private HttpMessageConverters converters;
+//	@Autowired
+//	private HttpMessageConverters converters;
+
+	private ApplicationContext applicationContext;
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
 
 	@Override
 	public void afterPropertiesSet() throws Exception {
+		if (applicationContext != null) {
+			if (applicationContext instanceof ConfigurableApplicationContext) {
+				ConfigurableListableBeanFactory beanFactory = ((ConfigurableApplicationContext) applicationContext)
+						.getBeanFactory();
+
+				Arrays.asList(BeanFactoryUtils.beanNamesForTypeIncludingAncestors(beanFactory, Object.class))
+						.forEach(System.err::println);
+			}
+		}
 //		System.err.println("111111111111111111111111111111111" + converters);
-		converters.forEach(c -> {
-			System.err.println(c.getClass().toString() + c.getSupportedMediaTypes());
-		});
+//		converters.forEach(c -> {
+//			System.err.println(c.getClass().toString() + c.getSupportedMediaTypes());
+//		});
 
 	}
 
